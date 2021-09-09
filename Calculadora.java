ccb349a466a1c33dc85c4e2a4bdb81fd4eb7bc7f
matheusmotaandrelino <40411851+matheusmotaandrelino@users.noreply.github.com>
2021-09-09 00:03:29 -0300

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,47 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in );
+		
+		float a, b;
+		
+		System.out.println("Digite o primeiro valor: ");
+		a = scan.nextFloat();
+		System.out.println("Digite o segundo valor: ");
+		b = scan.nextFloat();
+		
+		float soma = soma(a,b);
+		float subtracao = subtracao(a,b);
+		float divisao = divisao(a,b);
+		float multiplicacao = multiplicacao(a,b);
+		
+		System.out.println(soma);
+		System.out.println(subtracao);
+		System.out.println(divisao);
+		System.out.println(multiplicacao);
+	}
+	
+
+
+
+	public static float soma(float a, float b) {
+		return a + b;
+	}
+	public static float subtracao(float a, float b) {
+		return a - b;
+	}
+	public static float divisao(float a, float b) {
+		return a / b;
+	}
+	public static float multiplicacao(float a, float b) {
+		return a * b;
+	}
+	
+	
+	
+}
